c15501b2a68625af6b5791907cebc64a895ea0d4
ishiwadak <j118004p@st.u-gakugei.ac.jp>
2015-07-29 23:33:01 +0900

    さらにさらに改善

--- WEB-INF/src/validator/ItemValidator.java
+++ WEB-INF/src/validator/ItemValidator.java
@@ -5,34 +5,34 @@ public class ItemValidator {
 	private final int MAX_LENGTH_ITEM_NAME = 50;
 	private final int MAX_DIGIT_ITEM_PRICE = 12; 
 	private final int MAX_DIGIT_ITEM_STOCK = 3;  
-	private String ERROR_ITEM_NAME_TOO_LONG = "商品名は" + Integer.toString(MAX_LENGTH_ITEM_NAME) + "以内でご入力下さい。";
-	private String ERROR_ITEM_PRICE_TOO_MUCH = "単価は" + Integer.toString(MAX_DIGIT_ITEM_PRICE) + "桁以内でご入力下さい。";
-	private String ERROR_ITEM_PRICE_NOT_NUM = "単価は半角数字でご入力下さい。";
-	private String ERROR_ITEM_STOCK_TOO_MUCH = "在庫数は" + Integer.toString(MAX_DIGIT_ITEM_STOCK) + "桁以内でご入力下さい。";
-	private String ERROR_ITEM_STOCK_NOT_NUM = "在庫数は半角数字でご入力下さい。";
+	private String errorItemNameTooLong = "商品名は" + Integer.toString(MAX_LENGTH_ITEM_NAME) + "以内でご入力下さい。";
+	private String errorItemPriceTooMuch = "単価は" + Integer.toString(MAX_DIGIT_ITEM_PRICE) + "桁以内でご入力下さい。";
+	private String errorItemPriceNotNum = "単価は半角数字でご入力下さい。";
+	private String errorItemStockTooMuch = "在庫数は" + Integer.toString(MAX_DIGIT_ITEM_STOCK) + "桁以内でご入力下さい。";
+	private String errorItemStockNotNum = "在庫数は半角数字でご入力下さい。";
 	
 	public ItemValidator(){}
 	
 	public String validateItemName(String itemName){
-		return ( itemName.length() > MAX_LENGTH_ITEM_NAME || itemName.length() < 0 ) ? ERROR_ITEM_NAME_TOO_LONG : null;
+		return ( itemName.length() > MAX_LENGTH_ITEM_NAME || itemName.length() < 0 ) ? errorItemNameTooLong : null;
 	}
 
 	public String validateItemPrice(String itemPrice){
 		if( !isNumber(itemPrice)){
-			return ERROR_ITEM_PRICE_NOT_NUM;
+			return errorItemPriceNotNum;
 		}
 		else if(itemPrice.length() > MAX_DIGIT_ITEM_PRICE || itemPrice.length() < 0){
-			return ERROR_ITEM_PRICE_TOO_MUCH;
+			return errorItemPriceTooMuch;
 		}
 		return null;
 	}
 	
 	public String validateItemStock(String itemStock){
 		if( !isNumber(itemStock)){
-			return ERROR_ITEM_STOCK_TOO_MUCH;
+			return errorItemStockTooMuch;
 		}
 		else if(itemStock.length() > MAX_DIGIT_ITEM_STOCK || itemStock.length() < 0){
-			return ERROR_ITEM_STOCK_NOT_NUM;
+			return errorItemStockNotNum;
 		}
 		return null;
 	}
