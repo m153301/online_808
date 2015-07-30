f62da2fc75a9906acee1fbc256ecc6aa214728ea
m153301 <m153301s@u-gakugei.ac.jp>
2015-07-30 18:28:46 +0900

    static修飾子追加
    色々変わって申し訳ない..

--- WEB-INF/src/validator/ItemValidator.java
+++ WEB-INF/src/validator/ItemValidator.java
@@ -1,38 +1,54 @@
 package validator;
 
 public class ItemValidator {
+	private static final int MAX_LENGTH_ITEM_NAME = 50;
+	private static final int MAX_DIGIT_ITEM_PRICE = 12; 
+	private static final int MAX_DIGIT_ITEM_STOCK = 3;  
+	private static final int MIN_LENGTH_ITEM_NAME = 1;
+	private static final int MIN_DIGIT_ITEM_PRICE = 1; 
+	private static final int MIN_DIGIT_ITEM_STOCK = 1;  
 
-	private final int MAX_LENGTH_ITEM_NAME = 50;
-	private final int MAX_DIGIT_ITEM_PRICE = 12; 
-	private final int MAX_DIGIT_ITEM_STOCK = 3;  
-	private String errorItemNameTooLong = "商品名は" + Integer.toString(MAX_LENGTH_ITEM_NAME) + "以内でご入力下さい。";
-	private String errorItemPriceTooMuch = "単価は" + Integer.toString(MAX_DIGIT_ITEM_PRICE) + "桁以内でご入力下さい。";
-	private String errorItemPriceNotNum = "単価は半角数字でご入力下さい。";
-	private String errorItemStockTooMuch = "在庫数は" + Integer.toString(MAX_DIGIT_ITEM_STOCK) + "桁以内でご入力下さい。";
-	private String errorItemStockNotNum = "在庫数は半角数字でご入力下さい。";
+	private static final String ERROR_ITEM_ID_NULL = "商品を選択して下さい。";
+	private static final String ERROR_ITEM_NAME_TOO_LONG = "商品名は" + Integer.toString(MAX_LENGTH_ITEM_NAME) + "以内でご入力下さい。";
+	private static final String ERROR_ITEM_PRICE_TOO_MUCH = "単価は" + Integer.toString(MAX_DIGIT_ITEM_PRICE) + "桁以内でご入力下さい。";
+	private static final String ERROR_ITEM_PRICE_NOT_NUM = "単価は半角数字でご入力下さい。";
+	private static final String ERROR_ITEM_PRICE_NEGATIVE_NUM = "単価は正数でご入力下さい。";
+	private static final String ERROR_ITEM_STOCK_TOO_MUCH = "在庫数は" + Integer.toString(MAX_DIGIT_ITEM_STOCK) + "桁以内でご入力下さい。";
+	private static final String ERROR_ITEM_STOCK_NOT_NUM = "在庫数は半角数字でご入力下さい。";
+	private static final String ERROR_ITEM_STOCK_NEGATIVE_NUM = "在庫数は正数でご入力下さい。";
 	
 	public ItemValidator(){}
 	
+	public String validateItemId(String itemId){
+		return (itemId == null) ? ERROR_ITEM_ID_NULL : null;
+	}
+
 	public String validateItemName(String itemName){
-		return ( itemName.length() > MAX_LENGTH_ITEM_NAME || itemName.length() < 0 ) ? errorItemNameTooLong : null;
+		return (itemName.length() > MAX_LENGTH_ITEM_NAME || itemName.length() < MIN_LENGTH_ITEM_NAME) ? ERROR_ITEM_NAME_TOO_LONG : null;
 	}
 
 	public String validateItemPrice(String itemPrice){
-		if( !isNumber(itemPrice)){
-			return errorItemPriceNotNum;
+		if(!isNumber(itemPrice)){
+			return ERROR_ITEM_PRICE_NOT_NUM;
 		}
-		else if(itemPrice.length() > MAX_DIGIT_ITEM_PRICE || itemPrice.length() < 0){
-			return errorItemPriceTooMuch;
+		else if(Integer.parseInt(itemPrice) < 0){
+			return ERROR_ITEM_PRICE_NEGATIVE_NUM;
+		}
+		else if(itemPrice.length() > MAX_DIGIT_ITEM_PRICE || itemPrice.length() < MIN_DIGIT_ITEM_PRICE){
+			return ERROR_ITEM_PRICE_TOO_MUCH;
 		}
 		return null;
 	}
 	
 	public String validateItemStock(String itemStock){
 		if( !isNumber(itemStock)){
-			return errorItemStockTooMuch;
+			return ERROR_ITEM_STOCK_TOO_MUCH;
 		}
-		else if(itemStock.length() > MAX_DIGIT_ITEM_STOCK || itemStock.length() < 0){
-			return errorItemStockNotNum;
+		else if(Integer.parseInt(itemStock) < 0){
+			return ERROR_ITEM_STOCK_NEGATIVE_NUM;
+		}
+		else if(itemStock.length() > MAX_DIGIT_ITEM_STOCK || itemStock.length() < MIN_DIGIT_ITEM_STOCK){
+			return ERROR_ITEM_STOCK_NOT_NUM;
 		}
 		return null;
 	}
