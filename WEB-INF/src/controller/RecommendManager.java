b40bbb2f8f013623084eb941240ab7522acac674
Nobusuke Kobayashi <m153305n@st.u-gakugei.ac.jp>
2015-08-14 08:31:24 +0900

    ついでにdateの型の変換をManagerに移した

--- WEB-INF/src/controller/RecommendManager.java
+++ WEB-INF/src/controller/RecommendManager.java
@@ -6,6 +6,7 @@ package controller;
 /*****************************************************************************/
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import dao.RecommendDAO;
@@ -25,16 +26,25 @@ public class RecommendManager {
 		RecommendDAO recommendDAO = new RecommendDAO();
 		existingRecommend = recommendDAO.selectRecommendByUserId(recommend.getUserId());
 		
+		//RecommendオブジェクトのDateはjava.util.Dateなのでjava.sql.dateに変換
+		Calendar cal = Calendar.getInstance();
+		cal.setTime( recommend.getDate() );
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		java.sql.Date sqlDate = new java.sql.Date(cal.getTimeInMillis());
+		
 		//すでに当該店員によっておすすめが登録されていれば、更新する。されていなければ新規に登録する
 		if(existingRecommend != null)
 		{
 			System.out.println("Update.");
-			recommendRegistResult = recommendDAO.updateRecommendByUserId( recommend.getItemId(), recommend.getUserId(), recommend.getDate() );
+			recommendRegistResult = recommendDAO.updateRecommendByUserId( recommend.getItemId(), recommend.getUserId(), sqlDate );
 		}
 		else
 		{
 			System.out.println("Insert.");
-			recommendRegistResult = recommendDAO.insertRecommend( recommend.getItemId(), recommend.getUserId(), recommend.getDate() );
+			recommendRegistResult = recommendDAO.insertRecommend( recommend.getItemId(), recommend.getUserId(), sqlDate );
 		}
 		
 		return recommendRegistResult;
